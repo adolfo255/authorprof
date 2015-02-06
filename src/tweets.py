c8988985bb07643ea6779f5b82177b90891c4141
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2015-02-06 01:11:42 -0600

    Generates json by 100 tweets

--- src/tweets.py
+++ src/tweets.py
@@ -3,21 +3,49 @@ import secret_keys
 import xml.etree.ElementTree as ET
 import argparse
 import os
+import json
 
 array_tweets = []
 auth = tweepy.OAuthHandler(secret_keys.APIKEY , secret_keys.APISECRET)
 auth.set_access_token(secret_keys.TOKENACCES,secret_keys.TOKENSECRET)
 
-def obtener_tweets(file):
+chunk=100
+
+def create_json(data,age=None,gender=None):
+    ids,text=zip(*data)
+    return {
+    'index': ids,
+    'columns': ['age','gender','text'],
+    'data': [
+        [age,gender,t]
+            for t in text]
+    }
+
+def obtener_tweets(file,truth):
     """lee el xml y agrega el tweet leido en nuestro arreglo de tweets"""
     tree = ET.parse(file)
     root = tree.getroot()
+    ids=[]
+    b,e=os.path.splitext(file)
     for document in root.iter('document'):
         id = document.attrib['id']
-        tweet = api.get_status(id).text
-        array_tweets.append(tweet)
+        ids.append(id)
+    for i in range(len(ids)/chunk):
+        ids_=[x for x in ids[i:(i+1)*chunk] if
+            len(x) >= 18 ]
+        tweets = api.statuses_lookup(ids_)
+        tweets = [(t.id,t.text) for t in tweets]
+        array_tweets.extend(tweets)
+    ids_=[x for x in ids[i*chunk:] if
+            len(x) >= 18 ]
+    tweets = api.statuses_lookup(ids_)
+    tweets = [(t.id,t.text) for t in tweets]
+    array_tweets.extend(tweets)
+    gender,age=truth[os.path.basename(b)]
+    json_info=create_json(array_tweets,gender=gender,age=age)
+    write_json(json_info,b+'.json')
 
-def write_json(name):
+def write_json(array_tweets,name):
     """Escribe el archivo json"""
     with open(name, "w") as json_file :
         json.dump(array_tweets, json_file)
@@ -32,18 +60,18 @@ if __name__ == "__main__":
     p = argparse.ArgumentParser("transform-tweets")
     p.add_argument("DIR",default=None,
         action="store", help="Directory with corpus")
-#    p.add_argument("KEYS",default=None,
-#        action="store", help="Twitter screet keys")
     p.add_argument("-v", "--verbose",
         action="store_true", dest="verbose",
         help="Verbose mode [Off]")
     opts = p.parse_args()
 
+    truth={}
+    for line in open(os.path.join(opts.DIR,'truth.txt')):
+        bits=line.split(':::')
+        truth[bits[0]]=(bits[1],bits[2])
+
     for root, dirs, files in os.walk(opts.DIR):
         for filename in files:
             if filename.endswith('.xml'):
-              obtener_tweets(os.path.join(root,filename))
-    if opts.DIR =='data/pan14_spanish/':
-        write_json("pan14_spanish.json")
-    else opts.DIR == 'data/pan14_english' :
-        write_json("pan14_english.json")
+                obtener_tweets(os.path.join(root,filename),truth)
+   
