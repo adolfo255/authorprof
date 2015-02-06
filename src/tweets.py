1fb2597362ac87700cc3f1c6bf228425c07251b8
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2015-02-06 12:20:13 -0600

    Minor fix to fetching tweets

--- src/tweets.py
+++ src/tweets.py
@@ -5,7 +5,6 @@ import argparse
 import os
 import json
 
-array_tweets = []
 auth = tweepy.OAuthHandler(secret_keys.APIKEY , secret_keys.APISECRET)
 auth.set_access_token(secret_keys.TOKENACCES,secret_keys.TOKENSECRET)
 
@@ -27,23 +26,32 @@ def obtener_tweets(file,truth):
     root = tree.getroot()
     ids=[]
     b,e=os.path.splitext(file)
+    i=0
+    array_tweets = []
     for document in root.iter('document'):
         id = document.attrib['id']
         ids.append(id)
     for i in range(len(ids)/chunk):
-        ids_=[x for x in ids[i:(i+1)*chunk] if
-            len(x) >= 18 ]
-        tweets = api.statuses_lookup(ids_)
+        ids_=[x for x in ids[i*chunk:(i+1)*chunk] if
+            len(x) ]
+        try:
+            tweets = api.statuses_lookup(ids_,True,True)
+            tweets = [(t.id,t.text) for t in tweets]
+            array_tweets.extend(tweets)
+        except tweepy.error.TweepError:
+            print ids_,os.path.basename(b)
+    ids_=[x for x in ids[(i+1)*chunk:] if
+            len(x) ]
+    if len(ids_)>0:
+        tweets = api.statuses_lookup(ids_,True,True)
         tweets = [(t.id,t.text) for t in tweets]
         array_tweets.extend(tweets)
-    ids_=[x for x in ids[i*chunk:] if
-            len(x) >= 18 ]
-    tweets = api.statuses_lookup(ids_)
-    tweets = [(t.id,t.text) for t in tweets]
-    array_tweets.extend(tweets)
-    gender,age=truth[os.path.basename(b)]
-    json_info=create_json(array_tweets,gender=gender,age=age)
-    write_json(json_info,b+'.json')
+    if len(array_tweets)>0:
+        gender,age=truth[os.path.basename(b)]
+        json_info=create_json(array_tweets,gender=gender,age=age)
+        write_json(json_info,b+'.json')
+    else:
+        print "Warning: Nothing recovered", os.path.basename(b)
 
 def write_json(array_tweets,name):
     """Escribe el archivo json"""
