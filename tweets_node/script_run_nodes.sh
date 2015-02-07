a82f6847a5d5002d0969a42bb97bf9f249e6e3cc
Rodrigo Sanabria <r.sanabriadc@gmail.com>
2015-02-07 05:22:03 +0000

    agregado el package.json y modificado el script

--- tweets_node/script_run_nodes.sh
+++ tweets_node/script_run_nodes.sh
@@ -9,20 +9,37 @@ if [ ! -d "$DIR2" ]
 then
 mkdir $DIR2
 fi
-DIR3=tweets_es/
+DIR3=../data/tweets_es/
 if [ ! -d "$DIR3" ]
 then
 mkdir $DIR3
 fi
-DIR4=tweets_en/
+DIR4=../data/tweets_en/
 if [ ! -d "$DIR4" ]
 then
 mkdir $DIR4
 fi
-node xmlReader.js ../data/pan14_english xmlToJson_en
-node xmlReader.js ../data/pan14_spanish xmlToJson_es
-FILES=xmlToJson_en/*
-for f in $FILES
+echo "instalando dependencias"
+npm install
+echo "dependencias instaladas"
+if [ -d "../data/pan14_english" ]
+then
+echo "Conviertiendo xml_en..."
+node xmlReader.js ../data/pan14_english/ xmlToJson_en/
+fi
+if [ -d "../data/pan14_spanish " ]
+then
+echo "Conviertiendo xml_es..."
+node xmlReader.js ../data/pan14_spanish/ xmlToJson_es/
+fi
+echo "Descargando y Escribiendo tweets en ingles..."
+FILES_EN=xmlToJson_en/*
+for f in $FILES_EN
 do 
-node readTweets.js $f
+node readTweets.js $f ../data/tweets_en/
 done
+#FILES_ES=xmlToJson_es/*
+#for f1 in $FILES_ES
+#do
+#node readTweets.js $f1 --/data/tweets_es/
+#done
