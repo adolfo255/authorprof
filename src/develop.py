b7bdd476b4856f36632606c07bb83952b9ad7db4
RiAdFiVi <ricardo.fierr@gmail.com>
2015-03-12 11:18:59 -0600

    version ricardo funcional

--- src/develop.py
+++ src/develop.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 # -*- coding: utf-8
 from __future__ import print_function
 
@@ -57,7 +57,8 @@ if __name__ == "__main__":
         truth[bits[0]]=bits[1:]
 
     # Lee las etiquetas
-    with open(os.path.join(opts.dir,feats[0]+'.idx'),'r') as idxf:
+    print(os.path.join(opts.dir,feats[0]+'.idx'))
+    with open(os.path.join(opts.dir,feats[0]+'.idx'),'rb') as idxf:
         ids = pickle.load(idxf)
 
     # Lee la matrix de features de disco
