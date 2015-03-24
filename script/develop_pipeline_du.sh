c588398b3cbcf4d91e401681cabeb60d22f05a80
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2015-03-24 12:30:39 -0600

    Adds options for age and personality

--- script/develop_pipeline_du.sh
+++ script/develop_pipeline_du.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+mode="gender"
+echo "Running training authorid"
+while getopts m: opt; do
+	case $opt in
+	m)
+	mode=$OPTARG
+	;;
+	esac
+	done
+
+
 rm feats/*.idx
 rm feats/*.dat
 
 #python src/ef_1grams.py --stopwords data/stop_words/stop_words_dutch.txt data/pan15/dutch
 python src/ef_tfidf.py --stopwords data/stop_words/stop_words_dutch.txt data/pan15/dutch
+python src/ef_links.py data/pan15/dutch
 
-python src/develop.py -v --estimators 1000 data/pan15/dutch
+python src/develop.py -m ${mode} -v --estimators 10000 data/pan15/dutch
