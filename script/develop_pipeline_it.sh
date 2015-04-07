7f68d7d69f773d5630d4e48be329c87a6e48e749
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2015-04-07 14:29:51 -0500

    Batch para ingles

--- script/develop_pipeline_it.sh
+++ script/develop_pipeline_it.sh
@@ -17,7 +17,11 @@ rm feats/*.dat
 python src/ef_tfidf.py --stopwords data/stop_words/stop_words_it.txt data/pan15/italian
 python src/ef_list_baseline.py -p lb_reyes data/pan15/italian data/SentimentAnalysisDict/it/Reyes/counterFactuality-ita.txt data/SentimentAnalysisDict/it/Reyes/temporalCompression-ita.txt
 python src/ef_list_frequency.py -p lf_reyes data/pan15/italian data/SentimentAnalysisDict/it/Reyes/counterFactuality-ita.txt data/SentimentAnalysisDict/it/Reyes/temporalCompression-ita.txt
-python src/ef_links.py data/pan15/italian
+#python src/ef_links.py data/pan15/italian
+
+# Emoticons y puntuación
+python src/ef_list_emoticons.py data/pan15/italian data/emoticons.txt
+python src/ef_list_punctuation.py data/pan15/italian data/punctuation.txt
 
 python src/ef_list_baseline.py -p lb_hu data/pan15/italian data/SentimentAnalysisDict/it/Hu-Liu/positives.txt data/SentimentAnalysisDict/it/Hu-Liu/negatives.txt
 python src/ef_list_frequency.py -p lf_hu data/pan15/italian data/SentimentAnalysisDict/it/Hu-Liu/positives.txt data/SentimentAnalysisDict/it/Hu-Liu/negatives.txt
