1b931e4ce5dad0a01a4db1fefe34caae14149253
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2015-04-14 00:50:22 -0500

    Recovers develop script

--- script/develop_all_du.sh
+++ script/develop_all_du.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mode="gender"
-est=400
+est=2000
 echo "Running training authorprof"
 while getopts m: opt; do
 	case $opt in
@@ -17,17 +17,32 @@ while getopts m: opt; do
 
 
 
-rm feats/*.idx
-rm feats/*.dat
+rm feats/*
+# ------------  Based on vocabulary
+# tfidf
+python src/ef_tfidf.py --stopwords data/stop_words/stop_words_du.txt data/pan15/dutch
 
-python src/ef_tfidf.py --stopwords data/stop_words/stop_words_dutch.txt data/pan15/dutch
+# Extrae links
 python src/ef_links.py data/pan15/dutch
+
+# Usando listas de polarity
+python src/ef_polarity.py data/pan15/dutch data/SentimentAnalysisDict/du/polarity-AFINN.txt
+
+# Emoticons y puntuación
 python src/ef_list_emoticons.py data/pan15/dutch data/emoticons.txt
 python src/ef_list_punctuation.py data/pan15/dutch data/punctuation.txt
 
+# Stadistica de corpus
+python src/ef_statistics.py -v data/pan15/dutch
+
+# POS
+python src/extract_text.py data/pan15/dutch/
+bash script/tag_dutch.sh data/pan15/dutch/
+python src/ef_pos.py --tag 2 data/pan15/dutch
+
+
 # gender
 python src/develop.py --estimators ${est} -v data/pan15/dutch
-#python src/develop.py --estimators 300  -m age  -v data/pan15/dutch
 python src/develop.py --estimators ${est}  -m ex -v data/pan15/dutch
 python src/develop.py --estimators ${est}  -m st -v data/pan15/dutch
 python src/develop.py --estimators ${est}  -m op -v data/pan15/dutch
--- script/develop_all_it.sh
+++ script/develop_all_it.sh
@@ -17,49 +17,49 @@ while getopts m: opt; do
 
 
 
-rm $2/*
+rm feats/*
 # ------------  Based on vocabulary
 # tfidf
-python src/ef_tfidf.py --stopwords data/stop_words/stop_words_it.txt $1
+python src/ef_tfidf.py --stopwords data/stop_words/stop_words_it.txt data/pan15/italian
 
 # Extrae links
-python src/ef_links.py $1
+python src/ef_links.py data/pan15/italian
 
 # ------------ Bades on lists
 # Usando listas de positivos y negativos
-python src/ef_list_baseline.py -p lb_reyes $1 data/SentimentAnalysisDict/it/Reyes/counterFactuality-ita.txt data/SentimentAnalysisDict/it/Reyes/temporalCompression-ita.txt
-python src/ef_list_frequency.py -p lf_reyes $1 data/SentimentAnalysisDict/it/Reyes/counterFactuality-ita.txt data/SentimentAnalysisDict/it/Reyes/temporalCompression-ita.txt
+python src/ef_list_baseline.py -p lb_reyes data/pan15/italian data/SentimentAnalysisDict/it/Reyes/counterFactuality-ita.txt data/SentimentAnalysisDict/it/Reyes/temporalCompression-ita.txt
+python src/ef_list_frequency.py -p lf_reyes data/pan15/italian data/SentimentAnalysisDict/it/Reyes/counterFactuality-ita.txt data/SentimentAnalysisDict/it/Reyes/temporalCompression-ita.txt
 
 # Usando listas de polarity
-python src/ef_polarity.py --deli '#' $1 data/SentimentAnalysisDict/it/affin-v1.txt
+python src/ef_polarity.py --deli '#' data/pan15/italian data/SentimentAnalysisDict/it/affin-v1.txt
 
 # Emoticons y puntuación
-python src/ef_list_emoticons.py $1 data/emoticons.txt
-python src/ef_list_emoticons.py $1  data/SentimentAnalysisDict/it/taboowords_it.txt
-python src/ef_list_punctuation.py $1 data/punctuation.txt
+python src/ef_list_emoticons.py data/pan15/italian data/emoticons.txt
+python src/ef_list_emoticons.py data/pan15/italian  data/SentimentAnalysisDict/it/taboowords_it.txt
+python src/ef_list_punctuation.py data/pan15/italian data/punctuation.txt
 
 # Sentiword
-python src/ef_sentiword.py $1 data/SentimentAnalysisDict/en/SWN/SentiWN_it.csv
+python src/ef_sentiword.py data/pan15/italian data/SentimentAnalysisDict/en/SWN/SentiWN_it.csv
 
 # Lista de Whissell
-python src/ef_wissell_t.py $1/ data/SentimentAnalysisDict/it/Whissell/whissell-v1.txt
+python src/ef_wissell_t.py data/pan15/italian/ data/SentimentAnalysisDict/it/Whissell/whissell-v1.txt
 
 # Stadistica de corpus
-python src/ef_statistics.py  $1
+python src/ef_statistics.py  data/pan15/italian
 
 # POS
-python src/extract_text.py $1/
-bash script/tag_italian.sh $1/
-python src/ef_pos.py --tag 2 $1
+python src/extract_text.py data/pan15/italian/
+bash script/tag_italian.sh data/pan15/italian/
+python src/ef_pos.py --tag 2 data/pan15/italian
 
 
 # gender
-python src/develop.py --estimators ${est} -v $1
-#python src/develop.py --estimators ${est}  -m age -w weights/en_gender.txt -v $1
-python src/develop.py --estimators ${est}  -m ex -v $1
-python src/develop.py --estimators ${est}  -m st -v $1
-python src/develop.py --estimators ${est}  -m op -v $1
-python src/develop.py --estimators ${est}  -m co -v $1
-python src/develop.py --estimators ${est}  -m agre -v $1
+python src/develop.py --estimators ${est} -v data/pan15/italian
+#python src/develop.py --estimators ${est}  -m age -w weights/en_gender.txt -v data/pan15/italian
+python src/develop.py --estimators ${est}  -m ex -v data/pan15/italian
+python src/develop.py --estimators ${est}  -m st -v data/pan15/italian
+python src/develop.py --estimators ${est}  -m op -v data/pan15/italian
+python src/develop.py --estimators ${est}  -m co -v data/pan15/italian
+python src/develop.py --estimators ${est}  -m agre -v data/pan15/italian
 
 
